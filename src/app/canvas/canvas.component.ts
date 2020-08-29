233198cfb8861348b7a7dd7dfe72638ed1e6844b
Antonios Valmas <antonivalmas@gmail.com>
2020-08-29 14:02:12 +0300

    Add canvas component

---
+++ src/app/canvas/canvas.component.ts
@@ -0,0 +1,129 @@
+import {
+  Component,
+  AfterViewInit,
+  ViewChild,
+  ElementRef,
+  Input,
+} from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { switchMap, takeUntil, pairwise } from 'rxjs/operators';
+import { DataService } from '../services/data.service';
+
+@Component({
+  selector: 'app-canvas',
+  templateUrl: './canvas.component.html',
+  styleUrls: ['./canvas.component.scss'],
+})
+export class CanvasComponent implements AfterViewInit {
+  @ViewChild('canvas') public canvas: ElementRef;
+
+  @Input() public width = 280;
+  @Input() public height = 280;
+
+  private cx: CanvasRenderingContext2D;
+
+  constructor(private dataService: DataService) {}
+
+  ngAfterViewInit(): void {
+    // get the context
+    const canvasEl: HTMLCanvasElement = this.canvas.nativeElement;
+    this.cx = canvasEl.getContext('2d');
+
+    // set the width and height
+    canvasEl.width = this.width;
+    canvasEl.height = this.height;
+
+    // set some default properties about the line
+    this.cx.lineWidth = 15;
+    this.cx.lineCap = 'round';
+    this.cx.strokeStyle = '#000';
+
+    // we'll implement this method to start capturing mouse events
+    this.captureEvents(canvasEl);
+  }
+
+  private captureEvents(canvasEl: HTMLCanvasElement) {
+    // this will capture all mousedown events from the canvas element
+    fromEvent(canvasEl, 'mousedown')
+      .pipe(
+        switchMap((e) => {
+          // after a mouse down, we'll record all mouse moves
+          return fromEvent(canvasEl, 'mousemove').pipe(
+            // we'll stop (and unsubscribe) once the user releases the mouse
+            // this will trigger a 'mouseup' event
+            takeUntil(fromEvent(canvasEl, 'mouseup')),
+            // we'll also stop (and unsubscribe) once the mouse leaves the canvas (mouseleave event)
+            takeUntil(fromEvent(canvasEl, 'mouseleave')),
+            // pairwise lets us get the previous value to draw a line from
+            // the previous point to the current point
+            pairwise()
+          );
+        })
+      )
+      .subscribe((res: [MouseEvent, MouseEvent]) => {
+        const rect = canvasEl.getBoundingClientRect();
+
+        // previous and current position with the offset
+        const prevPos = {
+          x: res[0].clientX - rect.left,
+          y: res[0].clientY - rect.top,
+        };
+
+        const currentPos = {
+          x: res[1].clientX - rect.left,
+          y: res[1].clientY - rect.top,
+        };
+
+        // this method we'll implement soon to do the actual drawing
+        this.drawOnCanvas(prevPos, currentPos);
+      });
+  }
+
+  private drawOnCanvas(
+    prevPos: { x: number; y: number },
+    currentPos: { x: number; y: number }
+  ) {
+    // incase the context is not set
+    if (!this.cx) {
+      return;
+    }
+
+    // start our drawing path
+    this.cx.beginPath();
+
+    // we're drawing lines so we need a previous position
+    if (prevPos) {
+      // sets the start point
+      this.cx.moveTo(prevPos.x, prevPos.y); // from
+
+      // draws a line from the start pos until the current position
+      this.cx.lineTo(currentPos.x, currentPos.y);
+
+      // strokes the current path with the styles we set earlier
+      this.cx.stroke();
+    }
+  }
+
+  private downSampleImage(imageData: Uint8ClampedArray): number {
+    return imageData.reduce((sum, val) => sum + val) / (imageData.length / 4);
+  }
+
+  public submit() {
+    Array(28).map((_, j) => {
+      return Array(28).map((_, i) => {
+        return this.downSampleImage(
+          this.cx.getImageData(i * 10, j * 10, 10, 10).data
+        );
+      });
+    });
+    for (let j = 0; j < 28; j++) {
+      for (let i = 0; i < 28; i++) {
+        this.downSampleImage(this.cx.getImageData(i * 10, j * 10, 10, 10).data);
+      }
+    }
+  }
+
+  public clear() {
+    this.cx.clearRect(0, 0, this.width, this.height);
+  }
+}
